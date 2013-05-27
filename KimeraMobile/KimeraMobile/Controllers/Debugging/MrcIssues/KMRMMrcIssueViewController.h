8fc6434db93aacf55d0a8ace0b3689689c7e87ae
Massimo Oliviero <massimo.oliviero@gmail.com>
2013-05-28 00:02:55 +0200

    Updated travis.yml and add new debugging folder

---
+++ KimeraMobile/KimeraMobile/Controllers/Debugging/MrcIssues/KMRMMrcIssueViewController.h
@@ -0,0 +1,13 @@
+//
+//  KMRMMrcIssueViewController.h
+//  KimeraMobile
+//
+//  Created by Massimo Oliviero on 5/27/13.
+//  Copyright (c) 2013 Massimo Oliviero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMRMMrcIssueViewController : UIViewController
+
+@end
